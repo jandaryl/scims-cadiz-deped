57a2b62a3136c0921ea5f36969ac982f2c5aad54
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2019-02-05 03:41:33 -0500

    Fix the Bug#0205201906 in categories page

--- resources/views/frontend/pages/categories.blade.php
+++ resources/views/frontend/pages/categories.blade.php
@@ -7,11 +7,13 @@
         <div class="tile is-ancestor">
           <div class="inline-flex flex-wrap md:ml-24 tile is-parent">
             @foreach($tags as $tag)
-              <div class="tile is-child">
-                <div class="flex flex-col items-center h-auto w-auto font-mono font-medium box mb-4 mr-4 ml-4 text-center">
-                  <a class="text-left lg:text-2xl md:text-xl sm:text-lg text-lg text-grey-darkest hover:underline" href="{{ route('blog.tag', $tag->slug) }}">{{ $tag->name }}</a>
+              @if(strpos($tag->name, 'School') === false && strpos($tag->name, 'Training') === false)
+                <div class="tile is-child">
+                  <div class="flex flex-col items-center h-auto w-auto font-mono font-medium box mb-4 mr-4 ml-4 text-center">
+                    <a class="text-left lg:text-2xl md:text-xl sm:text-lg text-lg text-grey-darkest hover:underline" href="{{ route('blog.tag', $tag->slug) }}">{{ $tag->name }}</a>
+                  </div>
                 </div>
-              </div>
+              @endif
             @endforeach
           </div>
         </div>
