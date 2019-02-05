3de044447073df593d08d39e9c7a5aa5e5f56904
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2019-02-04 22:09:28 -0500

    Remove the unused code in frontend js files

--- resources/js/frontend/app.js
+++ resources/js/frontend/app.js
@@ -1,18 +1,12 @@
 import loadClientScripts from './load-client-scripts'
 
-// Vue & axios
-import Vue from 'vue'
 import { axios } from '../axios-config'
 
 import 'babel-polyfill'
-import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm'
 
 import { createLocales } from '../vue-i18n-config'
 
 window.axios = axios
-
-// Bootstrap Vue
-Vue.use(BootstrapVue)
 
 export function createApp() {
   const i18n = createLocales(window.locale)
--- resources/js/frontend/load-client-scripts.js
+++ resources/js/frontend/load-client-scripts.js
@@ -1,10 +1,7 @@
 import '../fontawesome'
-import 'slick-carousel'
 import intlTelInput from 'intl-tel-input'
-import 'pwstrength-bootstrap/dist/pwstrength-bootstrap'
 import swal from 'sweetalert2'
 import WebFont from 'webfontloader'
-// import Turbolinks from 'turbolinks'
 
 /**
  * JS Settings App
@@ -18,8 +15,6 @@ window.swal = swal
 window.locale = $('html').attr('lang')
 
 export default createApp => {
-  // Turbolinks.start()
-
   /**
    * Font
    */
@@ -28,7 +23,6 @@ export default createApp => {
       families: ['Roboto']
     }
   })
-
   /**
    * Cookie Consent
    */
@@ -52,106 +46,4 @@ export default createApp => {
       }
     })
   })
-
-  // document.addEventListener('turbolinks:load', () => {
-  //   /**
-  //    * Vue Mounting
-  //    */
-  //   if (document.getElementById('app') !== null) {
-  //     const { app } = createApp()
-  //     app.$mount('#app')
-  //   }
-
-  //   /**
-  //    * Tel Input
-  //    */
-  //   document.querySelectorAll('input[type="tel"]').forEach(input => {
-  //     intlTelInput(input)
-  //   })
-
-  //   /**
-  //    * Bind all bootstrap tooltips
-  //    */
-  //   $('[data-toggle="tooltip"]').tooltip()
-
-  //   /**
-  //    * Bind all bootstrap popovers
-  //    */
-  //   $('[data-toggle="popover"]').popover()
-
-  //   /**
-  //    * Slick
-  //    */
-  //   $('[data-toggle="slider"]')
-  //     .not('.slick-initialized')
-  //     .slick({
-  //       dots: true,
-  //       infinite: true,
-  //       speed: 300,
-  //       slidesToShow: 3,
-  //       slidesToScroll: 3,
-  //       responsive: [
-  //         {
-  //           breakpoint: 768,
-  //           settings: {
-  //             slidesToShow: 2
-  //           }
-  //         },
-  //         {
-  //           breakpoint: 576,
-  //           settings: {
-  //             slidesToShow: 1
-  //           }
-  //         }
-  //       ]
-  //     })
-
-  //   /**
-  //    * Bind all swal confirm buttons
-  //    */
-  //   $('[data-toggle="confirm"]').click(e => {
-  //     e.preventDefault()
-
-  //     window
-  //       .swal({
-  //         title: $(e.currentTarget).attr('data-trans-title'),
-  //         type: 'warning',
-  //         showCancelButton: true,
-  //         cancelButtonText: $(e.currentTarget).attr('data-trans-button-cancel'),
-  //         confirmButtonColor: '#dd4b39',
-  //         confirmButtonText: $(e.currentTarget).attr(
-  //           'data-trans-button-confirm'
-  //         )
-  //       })
-  //       .then(result => {
-  //         if (result.value) {
-  //           $(e.target)
-  //             .closest('form')
-  //             .submit()
-  //         }
-  //       })
-  //   })
-
-  //   $('[data-toggle="password-strength-meter"]').pwstrength({
-  //     ui: {
-  //       bootstrap4: true
-  //     }
-  //   })
-
-  //   /**
-  //    * Bootstrap tabs nav specific hash manager
-  //    */
-  //   let hash = document.location.hash
-  //   let tabanchor = $('.nav-tabs a:first')
-
-  //   if (hash) {
-  //     tabanchor = $(`.nav-tabs a[href="${hash}"]`)
-  //   }
-
-  //   tabanchor.tab('show')
-
-  //   $('a[data-toggle="tab"]').on('show.bs.tab', e => {
-  //     window.location.hash = e.target.hash
-  //   })
-  // })
 }
