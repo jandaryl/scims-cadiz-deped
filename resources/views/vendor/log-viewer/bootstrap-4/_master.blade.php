4db771132fcff5c567f83b1aaabe55d017a63fc7
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2019-02-07 09:10:11 -0500

    Remove the footer in log-viewer page

--- resources/views/vendor/log-viewer/bootstrap-4/_master.blade.php
+++ resources/views/vendor/log-viewer/bootstrap-4/_master.blade.php
@@ -242,18 +242,6 @@
         </main>
     </div>
 
-    {{-- Footer --}}
-    <footer class="main-footer">
-        <div class="container-fluid">
-            <p class="text-muted pull-left">
-                LogViewer - <span class="badge badge-info">version {{ log_viewer()->version() }}</span>
-            </p>
-            <p class="text-muted pull-right">
-                Created with <i class="fa fa-heart"></i> by ARCANEDEV <sup>&copy;</sup>
-            </p>
-        </div>
-    </footer>
-
     {{-- Scripts --}}
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" crossorigin="anonymous"></script>
