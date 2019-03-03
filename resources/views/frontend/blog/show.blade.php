0417e744d5e0cddb349deacbd437bcfc875862cb
Jan Daryl Galbo <jandarylgalbo@gmail.com>
2019-03-03 02:55:07 -0500

    Update the download button for pdf export

--- resources/views/frontend/blog/show.blade.php
+++ resources/views/frontend/blog/show.blade.php
@@ -17,7 +17,11 @@
     <!-- <a href="{{ route('export.pdf', ['post' => $post->slug]) }}">Export as PDF</a> -->
     <div class="container">
 
-    <a href="{{ route('export.pdf', $post->id) }}">Download as PDF</a>
+    <div class="flex float-right">
+        <button class="bg-blue hover:bg-blue-dark text-white text-sm font-bold py-2 px-4 h-10 w-auto rounded mb-4">
+            <a class="text-white hover:text-white" href="{{ route('export.pdf', $post->id) }}"><i class="fas fa-download fa-1x mr-2"></i>Download</a>
+        </button>
+    </div>
     </div>
     <nav class="nav mt-8">
         <span class="navbar-brand lg:text-lg md:text-lg sm:text-base text-base">@lang('labels.frontend.blog.tags')</span>
